5554bd80e4e2602ae8da7c6ca43d4849cfc1fece
lucas gorini <lucasgori123@gmail.com>
2025-06-25 13:46:40 -0300

    feat: add AOS library for animations and update package dependencies

---
+++ src/scripts/main.ts
@@ -0,0 +1,9 @@
+// src/scripts/main.ts
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // ¡Importante! Importamos los estilos de AOS aquí
+
+AOS.init({
+    duration: 800,
+    once: true,
+});
