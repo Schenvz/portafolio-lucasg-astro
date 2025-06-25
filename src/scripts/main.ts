3a25f87d6a2162967d8c6de7aa3e964149f315dc
lucas gorini <lucasgori123@gmail.com>
2025-06-25 14:35:45 -0300

    feat: implement AOS animations for improved user experience and remove unused AOS imports

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,13 @@
 // src/scripts/main.ts
 
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // ¡Importante! Importamos los estilos de AOS aquí
+import 'aos/dist/aos.css';
 
-AOS.init({
+// Le decimos al navegador: "Espera a que todo el contenido HTML de la página esté listo..."
+document.addEventListener('DOMContentLoaded', () => {
+  // "...y SÓLO ENTONCES, inicializa las animaciones."
+  AOS.init({
     duration: 800,
     once: true,
+  });
 });
